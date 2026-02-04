0cc571f1a11febaa848fb4db1ec9e124d0c4c9fb
Federico Bica <fedebicasua@gmail.com>
2026-02-04 00:01:37 -0300

    fix6

--- src/actions/product/delete-product.ts
+++ src/actions/product/delete-product.ts
@@ -5,20 +5,25 @@ import { revalidatePath } from 'next/cache';
 
 export const deleteProduct = async (id: string) => {
   try {
-    // 1. Podrías buscar las imágenes primero para borrarlas de Cloudinary si quisieras
-    
-    // 2. Borrar de la base de datos
+    // 1. Borramos primero las imágenes relacionadas
+    await prisma.productImage.deleteMany({
+      where: { productId: id }
+    });
+
+    // 2. Ahora que el producto no tiene "hijos", lo podemos borrar
     await prisma.product.delete({
       where: { id }
     });
 
     revalidatePath('/admin/products');
+    revalidatePath('/');
+    
     return { ok: true };
   } catch (error) {
     console.log(error);
     return {
       ok: false,
-      message: 'No se pudo eliminar el producto (puede que tenga órdenes asociadas)'
+      message: 'No se pudo eliminar el producto. Verifique si tiene órdenes asociadas.'
     };
   }
 };
